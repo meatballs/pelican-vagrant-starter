ae670376cec30be70c7eaaac08eb0fc70e8abeb8
Owen Campbell <owen.campbell@empiria.co.uk>
2014-10-01 15:39:22 +0100

    changes restart to start and removes python-web-server start

--- provision/install_pelican.sh
+++ provision/install_pelican.sh
@@ -3,5 +3,4 @@
 apt-get install -y python-pip
 pip install -r /vagrant/provision/requirements.txt
 cp /vagrant/provision/*.conf /etc/init
-restart pelican-server
-restart python-web-server
+start pelican-server
