6ac55af6aa1b03a154bc83f710ec7bf6f1d1bac7
Owen Campbell <owen.campbell@empiria.co.uk>
2014-10-01 16:15:33 +0100

    replaces pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Joe Bloggs'
-SITENAME = u'My Blog'
-SITEURL = 'http://myblog.me.uk'
+AUTHOR = u'Owen'
+SITENAME = u'Owen Campbell'
+SITEURL = 'http://owencampbell.me.uk'
 
 PATH = 'content'
 
@@ -17,16 +17,22 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
+# Blogroll
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
+
 SOCIAL = (
-    ('Google+', 'http://plus.google.com/+username',
+    ('Google+', 'http://plus.google.com/+OwenCampbell1',
         'fa fa-google-plus-square fa-fw fa-lg'),
-    ('Twitter', 'https://twitter.com/username',
+    ('Twitter', 'https://twitter.com/opcampbell',
         'fa fa-twitter-square fa-fw fa-lg'),
-    ('LinkedIn', 'https://www.linkedin.com/in/username',
+    ('LinkedIn', 'https://www.linkedin.com/in/owencampbell',
         'fa fa-linkedin-square fa-fw fa-lg'),
-    ('BitBucket', 'http://bitbucket.org/username',
+    ('BitBucket', 'http://bitbucket.org/meatballs',
         'fa fa-bitbucket-square fa-fw fa-lg'),
-    ('GitHub', 'http://github.com/username',
+    ('GitHub', 'http://github.com/meatballs',
         'fa fa-github-square fa-fw fa-lg'),)
 
 DEFAULT_PAGINATION = 10
@@ -34,9 +40,9 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
-PLUGIN_PATHS = ('plugins',)
+PLUGIN_PATHS = ('pelican-plugins',)
 PLUGINS = ['creole_reader', ]
 
 THEME = 'themes/voidy-bootstrap/'
 
-# DISQUS_SITENAME = 'sitename'
+DISQUS_SITENAME = 'owencampbell'
