1a0bdd94c9e69470a454e7f197308a55edd2ff5a
Owen Campbell <owen.campbell@empiria.co.uk>
2014-10-02 13:45:36 +0100

    corrects dev url

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Owen'
 SITENAME = u'Owen Campbell'
-SITEURL = 'http://owencampbell.me.uk'
+SITEURL = 'http://localhost:8000'
 
 PATH = 'content'
 
